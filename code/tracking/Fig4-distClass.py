9b865a54ed43dd6a894bda1a41f56d1b3f37ac27
HoChan Cheon <hcheon90@gmail.com>
2021-09-10 09:25:59 +0100

    Add delayed Exp and delayed Lognormal for Fig4

--- code/tracking/Fig4-distClass.py
+++ code/tracking/Fig4-distClass.py
@@ -1,5 +1,5 @@
 """
-Last update: 16-May-2021
+Last update: 2-September-2021
 
 Bayesian inference to select the best distribution class (i.e. model selection) given the observed times
 [Output] Fig4 in the main article; FigS4, FigS5 in the Supplementary Material
@@ -35,7 +35,6 @@ except AssertionError as ae:
 	print("seaborn==0.10.1")
 	sys.exit()
 
-
 # GLOBAL PLOT SETTINGS
 rc = {
 	'figure.figsize': (8, 6),
@@ -52,12 +51,6 @@ rc = {
 	'errorbar.capsize': 2.5
 }
 sns.set(context='paper', style='white', rc=rc)
-
-def confidence_band(x):
-	mean = np.mean(x, axis=0)
-	low = np.percentile(x, 2.5, interpolation='nearest', axis=0)
-	high = np.percentile(x, 97.5, interpolation='nearest', axis=0)
-	return mean, low, high
 
 VAR_MAP = {'tdiv0': "$T_{div}^0$", 'tdiv': "$T_{div}^{k\geq1}$", 'tld': "$T_{ld}$", 'tdie': "$T_{die}$"}
 DATA_PROPERTY = {
@@ -119,6 +112,49 @@ DATA_PROPERTY = {
 		'condition': "N4"
 	}
 }
+
+
+class DelayedLognormal(pm.Continuous):
+	def __init__(self, mu=0.0, tau=None, sigma=None, c=0.0, *args, **kwargs):
+		super(DelayedLognormal, self).__init__(*args, **kwargs)
+		self.mu = mu
+		self.c = c
+		self.tau, self.sigma = pm.distributions.continuous.get_tau_sigma(tau=tau, sigma=sigma)
+	
+	def logp(self, value):
+		mu = self.mu
+		tau = self.tau
+		x = value - self.c
+
+		bound = pm.distributions.continuous.bound
+		return bound(-0.5*tau*(pm.math.log(x) - mu)**2 + \
+					 0.5*pm.math.log(tau/(2.*np.pi)) - \
+					 pm.math.log(x),
+					 tau > 0, x > 0)
+
+class DelayedExponential(pm.Continuous):
+	def __init__(self, lam=None, c=0.0, *args, **kwargs):
+		super(DelayedExponential, self).__init__(*args, **kwargs)
+		self.lam = lam
+		self.c = c
+	
+	def logp(self, value):
+		lam = self.lam
+		x = value - self.c
+
+		bound = pm.distributions.continuous.bound
+		return bound(pm.math.log(lam) - lam * x, lam > 0, x > 0)
+	
+def get_dexp_cdf_from_posterior(x, lamb, c):
+	s = []
+	for l, t in zip(lamb.values, c.values):
+		s.append([1. - np.exp(-l * (xx - t)) if xx >= t else 0. for xx in x.values.ravel()])
+	return np.array(s)
+
+def confidence_band(x):
+	mean = np.mean(x, axis=0)
+	low, high = np.percentile(x, q=[2.5, 97.5], interpolation='nearest', axis=0)
+	return mean, low, high
 
 def _waic(trace, name, idx=0):
 	log_likelihood = trace.log_likelihood[list(trace.log_likelihood.data_vars)[0]]
@@ -191,26 +227,29 @@ if __name__ == "__main__":
 		max_time = max(df.max())
 		xrange = xr.DataArray(np.linspace(0, max_time, dt), dims="x")
 
-		lstys = ['-', '--', '-.', ':']
+		lstys = ['-', '--', '-.', ':', (0, (3, 5, 1, 5, 1, 5)), (0, (3, 1, 1, 1, 1, 1))]
 		colors = ['blue', 'orange', 'green', 'red']
-		fig1, axes = plt.subplots(nrows=3, ncols=1, sharex=True)
+		fig1, axes = plt.subplots(nrows=4, ncols=1, sharex=True)
 		axes[0].set_xlim(left=0, right=max_time)
-		axes[2].set_xlabel("Time (hour)")
+		axes[3].set_xlabel("Time (hour)")
 
 		fig2, ax2 = plt.subplots(nrows=2, ncols=2)
 		ax2 = ax2.flat
 		for i, (var, data) in enumerate(df.items()):
 			print(f'------------------------ BEGIN {var} ({fname}) ------------------------')
 			# Decide which axis to draw
-			if var in ['tdiv0', 'tdiv']: 
+			if var  == 'tdiv0': 
 				ploc = 0
-				axes[0].set_title(f"Time to first division ({VAR_MAP['tdiv0']}) & Avg. sub div time ({VAR_MAP['tdiv']})", x=0.01, ha='left', fontsize=13.5)
-			elif var == 'tld':
+				axes[0].set_title(f"Time to first division ({VAR_MAP['tdiv0']})", x=0.01, ha='left', fontsize=13.5)
+			elif var == 'tdiv':
 				ploc = 1
-				axes[1].set_title(f"Time to last division ({VAR_MAP['tld']})", x=0.01, ha='left', fontsize=13.5)
-			elif var == 'tdie': 
+				axes[1].set_title(f"Avg. sub div time ({VAR_MAP['tdiv']})", x=0.01, ha='left', fontsize=13.5)
+			elif var == 'tld':
 				ploc = 2
-				axes[2].set_title(f"Time to death ({VAR_MAP['tdie']})", x=0.01, ha='left', fontsize=13.5)
+				axes[2].set_title(f"Time to last division ({VAR_MAP['tld']})", x=0.01, ha='left', fontsize=13.5)
+			elif var == 'tdie': 
+				ploc = 3
+				axes[3].set_title(f"Time to death ({VAR_MAP['tdie']})", x=0.01, ha='left', fontsize=13.5)
 			axes[ploc].set_ylabel("eCDF")
 
 			if i == 0: ax2[0].set_title(f"Time to first division ({VAR_MAP['tdiv0']})", fontdict={'color': 'blue'})
@@ -255,8 +294,8 @@ if __name__ == "__main__":
 					lambda alpha, beta, x: sps.gamma(a=alpha, scale=1/beta).cdf(x),
 					post["alpha1"], post["beta1"], xrange
 				)
+
 				## Plot results, for proper plotting, "x" dim must be the first
-				# axes[0].plot(xrange, cdfs.transpose("x", ...), ls='-', lw=1, alpha=0.2)
 				gamma_mean, gamma_low, gamma_high = confidence_band(gamma_cdfs)
 				axes[ploc].plot(xrange, gamma_mean, ls=lstys[0], color=colors[i], label=f"Gam({gamma_summary.at['alpha1', 'mean']:.2f}, {1/gamma_summary.at['beta1', 'mean']:.2f})")
 				axes[ploc].plot(xrange, gamma_low, ls=lstys[0], lw=0.3, color=colors[i])
@@ -284,13 +323,13 @@ if __name__ == "__main__":
 				lnorm_waic = az.waic(lnorm_trace, lnorm_model, scale='deviance')
 				lnorm_summary = pm.summary(lnorm_trace)
 				
-				## Get random subset of the posterior
 				idx = rng.choice(lnorm_trace.posterior.mu1.size, nsubsample)
 				post = lnorm_trace.posterior.stack(sample=("chain", "draw")).isel(sample=idx)
 				lnorm_cdfs = xr.apply_ufunc(
 					lambda mu, sigma, x: sps.lognorm(s=sigma, scale=np.exp(mu)).cdf(x),
 					post["mu1"], post["sigma1"], xrange
 				)
+
 				lnorm_mean, lnorm_low, lnorm_high = confidence_band(lnorm_cdfs)
 				axes[ploc].plot(xrange, lnorm_mean, ls=lstys[1], color=colors[i], label=f"LN({np.exp(lnorm_summary.at['mu1', 'mean']):.2f}, {lnorm_summary.at['sigma1', 'mean']:.2f})")
 				axes[ploc].plot(xrange, lnorm_low, ls=lstys[1], lw=0.3, color=colors[i])
@@ -318,7 +357,6 @@ if __name__ == "__main__":
 				norm_waic = az.waic(norm_trace, norm_model, scale='deviance')
 				norm_summary = pm.summary(norm_trace)
 
-				## Get random subset of the posterior
 				idx = rng.choice(norm_trace.posterior.mu2.size, nsubsample)
 				post = norm_trace.posterior.stack(sample=("chain", "draw")).isel(sample=idx)
 				norm_cdfs = xr.apply_ufunc(
@@ -330,7 +368,7 @@ if __name__ == "__main__":
 				axes[ploc].plot(xrange, norm_low, ls=lstys[2], lw=0.3, color=colors[i])
 				axes[ploc].plot(xrange, norm_high, ls=lstys[2], lw=0.3, color=colors[i])
 				axes[ploc].fill_between(xrange, norm_low, norm_high, ls=lstys[2], color=colors[i], ec=(1, 1, 1, 1), alpha=0.1)
-				print("> Resul NORMAL:")
+				print("> Result NORMAL:")
 				print(norm_summary, end='\n\n')
 
 				#########################################################################################################
@@ -354,7 +392,6 @@ if __name__ == "__main__":
 				weibull_waic = az.waic(weibull_trace, weibull_model, scale='deviance')
 				weibull_summary = pm.summary(weibull_trace)
 
-				## Get random subset of the posterior
 				idx = rng.choice(weibull_trace.posterior.alpha2.size, nsubsample)
 				post = weibull_trace.posterior.stack(sample=("chain", "draw")).isel(sample=idx)
 				weibull_cdfs = xr.apply_ufunc(
@@ -366,35 +403,107 @@ if __name__ == "__main__":
 				axes[ploc].plot(xrange, weibull_low, ls=lstys[3], lw=0.3, color=colors[i])
 				axes[ploc].plot(xrange, weibull_high, ls=lstys[3], lw=0.3, color=colors[i])
 				axes[ploc].fill_between(xrange, weibull_low, weibull_high, ls=lstys[3], color=colors[i], ec=(1, 1, 1, 1), alpha=0.1)
-				print("> Resul WEIBULL:")
+				print("> Result WEIBULL:")
 				print(weibull_summary, end='\n\n')
 
+				#########################################################################################################
+				# 									   DELAYED EXP DISTRIBUTION											#
+				#########################################################################################################
+				print("-->>>>>> DELAYED EXPONENTIAL")
+				with pm.Model() as dexp_model: 
+					lamb = pm.Uniform('lamb', lower=1E-10, upper=2.)
+					c = pm.Uniform('c', lower=0., upper=np.nanmin(data))
+
+					dexp = DelayedExponential('dexp', lam=lamb, c=c, observed=data)
+					dexp_trace = pm.sample(draws=niter,
+										   tune=ntune,
+										   chains=nchain,
+										   cores=ncore,
+										   random_seed=89475927,
+										   target_accept=0.95,
+										   init='jitter+adapt_diag',
+										   return_inferencedata=True)
+				dexp_waic = az.waic(dexp_trace, dexp_model, scale='deviance')
+				dexp_summary = pm.summary(dexp_trace)
+
+				idx = rng.choice(dexp_trace.posterior.lamb.size, nsubsample)
+				post = dexp_trace.posterior.stack(sample=("chain", "draw")).isel(sample=idx)
+				dexp_cdfs = get_dexp_cdf_from_posterior(xrange, post["lamb"], post["c"])
+				dexp_mean, dexp_low, dexp_high = confidence_band(dexp_cdfs)
+				axes[ploc].plot(xrange, dexp_mean, ls=lstys[4], color=colors[i], label=rf"dExp({dexp_summary.at['lamb', 'mean']:.2f}, {dexp_summary.at['c', 'mean']:.2f})")
+				axes[ploc].plot(xrange, dexp_low, ls=lstys[4], lw=0.3, color=colors[i])
+				axes[ploc].plot(xrange, dexp_high, ls=lstys[4], lw=0.3, color=colors[i])
+				axes[ploc].fill_between(xrange, dexp_low, dexp_high, ls=lstys[4], color=colors[i], ec=(0, 0, 0, 0), alpha=0.1)
+				print("> Result DELAYED EXPONENTIAL:")
+				print(dexp_summary, end='\n\n')
+
+				#########################################################################################################
+				# 									DELAYED LOGNORMAL DISTRIBUTION										#
+				#########################################################################################################
+				print("-->>>>>> DELAYED LOGNORMAL")
+				with pm.Model() as dLN_model: 
+					muLN = pm.Uniform('muLN', lower=1E-10, upper=200)
+					sigmaLN = pm.Uniform('sigmaLN', lower=1E-10, upper=200)
+					c = pm.Uniform('c', lower=0., upper=np.nanmin(data))
+					
+					dLN = DelayedLognormal('dLN', mu=muLN, sigma=sigmaLN, c=c, observed=data)
+					dLN_trace = pm.sample(draws=niter,
+										  tune=ntune,
+										  chains=nchain,
+										  cores=ncore,
+										  random_seed=55150623,
+									   	  target_accept=0.95,
+										  init='jitter+adapt_diag',
+										  return_inferencedata=True)
+				dLN_waic = az.waic(dLN_trace, dLN_model, scale='deviance')
+				dLN_summary = pm.summary(dLN_trace)
+
+				idx = rng.choice(dLN_trace.posterior.muLN.size, nsubsample)
+				post = dLN_trace.posterior.stack(sample=("chain", "draw")).isel(sample=idx)
+				dLN_cdfs = xr.apply_ufunc(
+					lambda mu, sigma, c, x: sps.lognorm(s=sigma, scale=np.exp(mu), loc=c).cdf(x),
+					post["muLN"], post["sigmaLN"], post["c"], xrange
+				)
+				dLN_mean, dLN_low, dLN_high = confidence_band(dLN_cdfs)
+				axes[ploc].plot(xrange, dLN_mean, ls=lstys[5], color=colors[i], 
+								label=rf"dLN({dLN_summary.at['muLN', 'mean']:.2f}, {dLN_summary.at['sigmaLN', 'mean']:.2f}, {dLN_summary.at['c', 'mean']:.2f})")
+				axes[ploc].plot(xrange, dLN_low, ls=lstys[5], lw=0.3, color=colors[i])
+				axes[ploc].plot(xrange, dLN_high, ls=lstys[5], lw=0.3, color=colors[i])
+				axes[ploc].fill_between(xrange, dLN_low, dLN_high, ls=lstys[5], color=colors[i], ec=(0, 0, 0, 0), alpha=0.1)
+				print("> Result DELAYED LOGNORAML:")
+				print(dLN_summary, end='\n\n')
+
+
+
 				axes[ploc].grid(True, which='major', axis='both', linestyle='--')
-				if ploc == 0:
-					axes[ploc].legend(loc='lower right', fontsize=11, ncol=2, columnspacing=0.2)
-				else:
-					axes[ploc].legend(loc='upper left', fontsize=11, ncol=1)
+				axes[ploc].legend(fontsize=9.5, ncol=2, columnspacing=0.5)
+				# if ploc == 0:
+				# 	axes[ploc].legend(loc='lower right', fontsize=9.5, ncol=2, columnspacing=0.2)
+				# else:
+				# 	axes[ploc].legend(loc='upper left', fontsize=9.5, ncol=1)
 
 				#########################################################################################################
 				# 								COMPARE DISTRIBUTIONS (WAIC)											#
 				#########################################################################################################
 				print("-->>>>>> MODEL SELECTION: WAIC")
-				my_waic = compare_waics([gamma_trace, lnorm_trace, norm_trace, weibull_trace], ['Gam', 'LN', 'N', 'Wei'])
+				my_waic = compare_waics([gamma_trace, lnorm_trace, norm_trace, weibull_trace, dexp_trace, dLN_trace], 
+										['Gam', 'LN', 'N', 'Wei', 'dExp', 'dLN'])
 				print(">>> Compare WAIC")
 				print(my_waic, end='\n')
 
-				# print(">>> PyMC3/Arviz WAIC")
 				dfwaic = az.compare(  # Identical to compare_waic() defined above. But it's convenient to use this for plotting the results
-					{'Gam': gamma_trace, 'LN': lnorm_trace, 'N': norm_trace, 'Wei': weibull_trace}, 
+					{'Gam': gamma_trace, 'LN': lnorm_trace, 'N': norm_trace, 'Wei': weibull_trace, 'dExp': dexp_trace, 'dLN': dLN_trace}, 
 					ic='WAIC', method='stacking', scale='deviance')
-				# print(dfwaic, end='\n\n')
-				plt.rcParams.update({'axes.titlesize': 14, 'axes.labelsize': 14,
-									'xtick.labelsize': 14, 'ytick.labelsize': 14})
+				print(">>> PyMC3/Arviz")
+				print(dfwaic, end='\n\n')
+				plt.rcParams.update({'axes.titlesize': 12, 'axes.labelsize': 12,
+									'xtick.labelsize': 12, 'ytick.labelsize': 12})
 				az.plot_compare(dfwaic, textsize=16, ax=ax2[i])
 				plt.rcParams.update({'axes.titlesize': rc['axes.titlesize'], 'axes.labelsize': rc['axes.labelsize'],
 									'xtick.labelsize': rc['xtick.labelsize'], 'ytick.labelsize': rc['ytick.labelsize']})
 			else:
 				print(f'CANNOT PROCEED. {var} HAS NO DATA!')
+				axes[ploc].annotate("NA", xy=(0.45, 0.5), xycoords='axes fraction', weight='bold', fontsize=16)
 				ax2[i].annotate("NA", xy=(0.45, 0.5), xycoords='axes fraction', weight='bold', fontsize=16)
 				ax2[i].set_xticklabels([])
 				ax2[i].set_yticklabels([])
@@ -404,7 +513,7 @@ if __name__ == "__main__":
 		ax2[0].set_xlabel("")
 		ax2[1].set_xlabel("")
 		fig1.tight_layout(rect=(0, 0, 1, 1))
-		fig1.subplots_adjust(hspace=0.17, wspace=0)
+		fig1.subplots_adjust(hspace=0.21, wspace=0)
 		fig2.tight_layout(rect=(0, 0, 1, 1))
 		fig2.subplots_adjust(hspace=0.255, wspace=0.2)
 
